2638097b1eb8e2e0771594596566155b0d69f94f
balovbohdan <balovbohdan@gmail.com>
2026-03-03 10:50:03 +0200

    id:47 feat(root-types): exported

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Changelog
 
+## [1.1.0] - 2026-03-03
+
+### [47 Export TypeScript Types for External Use](https://github.com/torqlab/strava-api/issues/47)
+
+### Added
+- Export `StravaApiConfig` and `StravaActivity` TypeScript types from the package index
+
 ## [1.0.0] - 2026-02-26
 
 ### Added
--- index.ts
+++ index.ts
@@ -3,3 +3,7 @@ export { default as fetchStravaActivities } from './fetch-activities';
 export { default as getStravaAuthUrl } from './get-auth-url';
 export { default as exchangeStravaAuthToken } from './exchange-token';
 export { default as refreshStravaAuthToken } from './refresh-token';
+export type {
+  StravaApiConfig,
+  StravaActivity,
+} from './types';
